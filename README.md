f5341378edfd1447767925e4c2ee996b3a9721d0
glebe <gleberphant@gmail.com>
2024-08-24 01:40:47 -0300

    implementei controle de uma bola

---
+++ README.md
@@ -0,0 +1,84 @@
+<div align="center">
+
+![BANNER](https://www.pygame.org/docs/_static/pygame_tiny.png)
+
+![PYTHON BADGE](	https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white)
+![GitHub repo size](https://img.shields.io/github/repo-size/iuricode/README-template?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/iuricode/README-template?style=for-the-badge)
+
+ 
+ 
+Desenvolvido por gr4v4t1nh4
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+ 
+</div>
+
+
+
+## 🐙 Descrição
+
+Estudos práticos da biblioteca PyGame. 
+
+## 💻 Pré-requisitos
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- *PYTHON 3.12*
+- *PyGame* 
+
+
+
+## 🚀 Instalação do projeto
+
+Em um terminal de comando digite:
+
+### 1 - Clonar o repositório
+
+```
+git clone https://github.com/gleberphant/pygame.git
+```
+### 2 - Executar Projeto
+
+```
+python main.py
+```
+
+## 📫 Contribuindo com o projeto
+
+Para contribuir com o projeto, siga estas etapas:
+
+1. Bifurque este repositório.
+2. Crie um branch: `git checkout -b <nome_branch>`.
+3. Faça suas alterações e confirme-as: `git commit -m '<mensagem_commit>'`
+4. Envie para o branch original: `git push origin <nome_do_projeto> / <local>`
+5. Crie a solicitação de pull.
+
+Consulte a documentação do GitHub em [como criar uma solicitação pull](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 😄 Autor
+
+HANDERSON GLEBER DE LIMA CAVALCANTI (GRAVATINHA)
+
+
+## 🤝 Colaboradores
+
+E- mail:  handerson.gleber@gmail.com
+
+https://www.instagram.com/handersongleber/
+
+## 📝 Licença
+
+### Released under MIT License
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copyright (c) 2013 Mark Otto.
+
+Copyright (c) 2017 Andrew Fong.
--- game_class.py
+++ game_class.py
@@ -1,29 +1,107 @@
 import pygame
 
-TAMANHO_JANELA: tuple = (800, 600)
-
 
 class Game:
+    # constantes e configurações
+    RESOLUCAO: tuple = (800, 600)
+    CENTRO_TELA: tuple = (RESOLUCAO[0] / 2, RESOLUCAO[1] / 2)
+    PARADO, CIMA, BAIXO, ESQUERDA, DIREITA = 0, 1, 2, 3, 4
+
+    # atributos: variáveis
+    delta_time: float
+
+    # atributos: objetos
+    jogador: dict = \
+        {
+            'sprite_surface': None,
+            'posicao': [0, 0],
+            'direcao': {'PARADO': False, 'CIMA': False, 'BAIXO': False, 'ESQUERDA': False, 'DIREITA': False},
+            'vivo': True,
+            'movimento': True
+        }
+
+    # construtor da classe
     def __init__(self):
         pygame.init()
-        self.tela = pygame.display.set_mode(TAMANHO_JANELA)
+        self.jogador['posicao'] = list(self.CENTRO_TELA)
+
+        self.tela = pygame.display.set_mode(self.RESOLUCAO)
         self.clock = pygame.time.Clock()
         self.game_state: bool = True
 
+    # método para lidar com o controle de eventos do jogo
     def handle_event(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.game_state = False
-                return False
+            match event.type:
+                case pygame.QUIT:
+                    self.game_state = False
+                    return False
+                case pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.game_state = False
+                        return False
+        teclas_pressionadas = pygame.key.get_pressed()
 
+        if teclas_pressionadas[pygame.K_UP] | teclas_pressionadas[pygame.K_w]:
+            self.jogador['movimento'] = True
+            self.jogador['direcao']['PARADO'] = False
+            self.jogador['direcao']['CIMA'] = True
+
+        if teclas_pressionadas[pygame.K_DOWN] | teclas_pressionadas[pygame.K_s]:
+
+            self.jogador['movimento'] = True
+            self.jogador['direcao']['PARADO'] = False
+            self.jogador['direcao']['BAIXO'] = True
+
+        if teclas_pressionadas[pygame.K_LEFT] | teclas_pressionadas[pygame.K_a]:
+
+            self.jogador['movimento'] = True
+            self.jogador['direcao']['PARADO'] = False
+            self.jogador['direcao']['ESQUERDA'] = True
+
+        if teclas_pressionadas[pygame.K_RIGHT] | teclas_pressionadas[pygame.K_d]:
+
+            self.jogador['movimento'] = True
+            self.jogador['direcao']['PARADO'] = False
+            self.jogador['direcao']['DIREITA'] = True
+
+        if teclas_pressionadas[pygame.K_ESCAPE]:
+            self.game_state = False
+            return False
         return True
 
+    # método para atualizar os dados dos objetos do jogo
     def update_states(self):
+        # verifica se jogador está em movimento
+        if not self.jogador['direcao']['PARADO']:
+            if self.jogador['direcao']['CIMA']:
+                self.jogador['posicao'][1] -= 3
+                self.jogador['direcao']['CIMA'] = False
+            if self.jogador['direcao']['BAIXO']:
+                self.jogador['posicao'][1] += 3
+                self.jogador['direcao']['BAIXO'] = False
+            if self.jogador['direcao']['ESQUERDA']:
+                self.jogador['posicao'][0] -= 3
+                self.jogador['direcao']['ESQUERDA'] = False
+            if self.jogador['direcao']['DIREITA']:
+                self.jogador['posicao'][0] += 3
+                self.jogador['direcao']['DIREITA'] = False
+
+            self.jogador['direcao']['PARADO'] = True
+            self.jogador['movimento'] = False
+
 
         return True
 
+    # método para desenhar a tela do jogo
     def draw_screen(self):
-        self.tela.fill("purple")
+        # limpa tela
+        self.tela.fill("white")
+
+        # desenha personagem
+        pygame.draw.circle(self.tela, "red", self.jogador['posicao'], 20)
+
+        # atualiza tela
         pygame.display.update()
         return True
 
@@ -39,8 +117,10 @@ class Game:
 
         while self.game_state:
             self.game_loop()
-            self.clock.tick(60)
+
+            # limits FPS to 60
+            # delta time in seconds since last frame, used for frame rate independent physics.
+            self.delta_time = self.clock.tick(60) / 1000
 
         pygame.quit()
         exit(0)
-
